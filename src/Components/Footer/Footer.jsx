2bfbb8b75bfabd81f4605f91bc71a74f035cc588
nayeong lee <lny021102@gmail.com>
2024-04-29 23:33:09 +0900

    [ADD] footer 추가, mainpage 일부 추가

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
+import { styled } from 'styled-components';
+import CustomColumn from '../Container/CustomColumn';
+
+const FooterContainer = styled.footer`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+width: 100%;
+height: 10vh;
+background-color: #575757;
+color: white;
+gap: 10px;
+`;
+
+const Detail = styled.a`
+font-size: 10px;
+font-family: 'RIDIBatang';
+color: white;
+`;
+
+const Out = styled.button`
+background: transparent;
+border: none;
+font-size: 10px;
+font-family: 'RIDIBatang';
+color: white;
+text-decoration: underline;
+`;
+
 export default function Component() {
     return (
-        <footer>
-            여기는 Footer
-        </footer>
-    )
+        <FooterContainer>
+            <CustomColumn width='100%' alignItems='center' gap='0.6rem'>
+                <Detail>내 문서를 부탁해</Detail>
+                <Out>서비스 탈퇴</Out>
+            </CustomColumn>
+        </FooterContainer>
+    );
 };
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,46 @@
+import CustomRow from "../../Components/Container/CustomRow";
+import styled from 'styled-components';
+import StyledImg from "../../Components/Container/StyledImg";
+import CustomFont from "../../Components/Container/CustomFont";
+import CustomColumn from "../../Components/Container/CustomColumn";
+
+const ContainerCenter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const PageContainer = styled(ContainerCenter)`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding-top: 8vh;
+  padding-bottom: 5vh;
+  gap: 20px;
+  position: relative;
+  background-color: white;
+`;
+
 export default function Component() {
   return (
-    <div>
-      <p> 여기는 MainPage! </p>
-    </div>
+    <ContainerCenter>
+      <PageContainer>
+        <CustomColumn width='100%' justifyContent='center' alignItems='center' gap='2rem'>
+
+          <CustomRow width='90%' justifyContent='center' alignItems='flex-end' gap='0.1rem'>
+            <StyledImg src={'icon_boo_small.png'} />
+            <StyledImg src={'icon_boo_big.png'} />
+            <StyledImg src={'icon_boo_middle.png'} />
+          </CustomRow>
+
+          <CustomFont fontWeight='bold' color='black' font='2rem'>각종 문서, AI가 생성해드릴게요!</CustomFont>
+
+        </CustomColumn>
+      </PageContainer>
+    </ContainerCenter>
   );
 };
