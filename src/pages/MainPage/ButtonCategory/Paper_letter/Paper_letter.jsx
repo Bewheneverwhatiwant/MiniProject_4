4c922c8a3b9c9c6eda82df9e3f7f59cfd9e99213
nayeong lee <lny021102@gmail.com>
2024-04-30 13:35:54 +0900

    [ADD] 모든 paper 화면 추가 완료(뼈대)

--- src/pages/MainPage/ButtonCategory/Paper_letter/Paper_letter.jsx
+++ src/pages/MainPage/ButtonCategory/Paper_letter/Paper_letter.jsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import CustomColumn from '../../../../Components/Container/CustomColumn';
+import CustomRow from '../../../../Components/Container/CustomRow';
+import CustomFont from '../../../../Components/Container/CustomFont';
+import StyledImg from '../../../../Components/Container/StyledImg';
+import React, { useState } from 'react';
 
 const ContainerCenter = styled.div`
   display: flex;
@@ -19,14 +24,140 @@ const PageContainer = styled(ContainerCenter)`
   gap: 20px;
   position: relative;
   background-color: white;
+
+  align-items: center;
+`;
+
+const InputForm = styled.input`
+  display: flex;
+  border: 1.5px solid #8CC63F;
+  background-color: transparent;
+  border-radius: 15px;
+
+  width: 100%;
+  height: 2rem;
+  padding: 0.3rem;
+
+  &::placeholder {
+    color: #D9D9D9;
+  }
+
+  &:active {
+    outline: none;
+  }
+`;
+
+const TextareaForm = styled.textarea`
+  display: flex;
+  border: 1.5px solid #8CC63F;
+  background-color: transparent;
+  border-radius: 15px;
+  width: 100%;
+  height: 4rem;
+  padding: 0.3rem;
+
+  &::placeholder {
+    color: #D9D9D9;
+  }
+
+  &:active {
+    outline: none;
+  }
+`;
+
+const SendButton = styled.button`
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  background-color: ${props => props.isActive ? '#8CC63F' : '#D9D9D9'};
+  cursor: ${props => props.isActive ? 'pointer' : 'not-allowed'};
+  pointer-events: ${props => props.isActive ? 'auto' : 'none'};
+`;
+
+const Boo_says = styled.div`
+  width: 30%;
+  height: 100px;
+  background-color: rgba(140, 198, 63, 0.5);
+  color: white;
+  border: none;
+  border-radius: 50%;
+  padding: 1rem;
+  font-size: 1rem;
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 0.8rem;
+  margin-top: 5px; // InputForm 바로 아래에 표시되도록 margin-top 설정
 `;
 
 export default function Component() {
-    return (
-        <ContainerCenter>
-            <PageContainer>
-                여기는 편지를 생성하는 페이지!
-            </PageContainer>
-        </ContainerCenter>
-    );
+  const [recipient, setRecipient] = useState('');
+  const [volume, setVolume] = useState('');
+  const [reason, setReason] = useState('');
+  const [volumeError, setVolumeError] = useState('');
+
+  const handleVolumeChange = (e) => {
+    const value = e.target.value;
+    setVolume(value);
+    if (value && !/^\d+$/.test(value)) {
+      setVolumeError('숫자만 입력해주세요');
+    } else if (parseInt(value, 10) > 3000) {
+      setVolumeError('3000자 이하로 설정해야 합니다');
+    } else {
+      setVolumeError('');
+    }
+  };
+
+  const isFormValid = recipient && volume && reason && !volumeError;
+
+  return (
+    <ContainerCenter>
+      <PageContainer>
+        <CustomColumn width='80%' gap='4rem' justify-content='center' align-items='center'>
+
+          <CustomRow width='100%' gap='1rem' justify-content='center' align-items='center' >
+            <StyledImg src={'icon_boo_glass.png'} />
+            <Boo_says>나에게 맡겨!</Boo_says>
+          </CustomRow>
+
+          <CustomColumn width='100%' justify-content='center' align-items='center'>
+
+            <CustomRow width='100%' justifyContent='flex-start' alignItems='center' gap='1rem'>
+              <CustomFont color='black' font='1rem'>문서를 보내는 대상을 설명해주세요</CustomFont>
+              <CustomFont color='red' font='1rem'>*</CustomFont>
+            </CustomRow>
+            <InputForm value={recipient} onChange={(e) => setRecipient(e.target.value)} />
+            {!recipient && <ErrorMessage>필수 필드입니다.</ErrorMessage>}
+
+            <CustomRow width='100%' justifyContent='flex-start' alignItems='center' gap='1rem'>
+              <CustomFont color='black' font='1rem'>문서의 분량을 설정해주세요</CustomFont>
+              <CustomFont color='red' font='1rem'>*</CustomFont>
+            </CustomRow>
+            <InputForm placeholder='3000자 이내로 설정해주세요.' value={volume} onChange={handleVolumeChange} />
+            {volumeError && <ErrorMessage>{volumeError}</ErrorMessage>}
+
+            <CustomRow width='100%' justifyContent='flex-start' alignItems='center' gap='1rem'>
+              <CustomFont color='black' font='1rem'>문서를 생성하는 이유를 알려주세요</CustomFont>
+              <CustomFont color='red' font='1rem'>*</CustomFont>
+            </CustomRow>
+            <TextareaForm value={reason} onChange={(e) => setReason(e.target.value)} />
+            {!reason && <ErrorMessage>필수 필드입니다.</ErrorMessage>}
+          </CustomColumn>
+
+          <SendButton isActive={isFormValid}>문서 생성하기</SendButton>
+        </CustomColumn>
+      </PageContainer>
+    </ContainerCenter>
+  );
 };
