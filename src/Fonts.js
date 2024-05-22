adaaded2c8c07cab3268c2c71eb587b36c38a61b
nayeong lee <lny021102@gmail.com>
2024-05-23 08:26:20 +0900

    [ADD] 웹 폰트 적용 완료

---
+++ src/Fonts.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+body {
+    font-family: 'BMJUA';
+}
+
+@font-face {
+    font-family: 'BMJUA';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMJUA.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import router from "./router"
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './pages/SubPage/AuthContext';
+import Fonts from './Fonts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthProvider>
+        <Fonts />
         <RouterProvider router={router} />
     </AuthProvider>
 );
