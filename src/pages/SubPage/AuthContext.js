0a981ea4aea1401210913ea76118b43e62939fe1
nayeong lee <lny021102@gmail.com>
2024-05-25 04:36:16 +0900

    [FIX] 로그인 시 프로필 img가 header에서 바로 로드되지 않는 오류 수정 완료

--- src/pages/SubPage/AuthContext.js
+++ src/pages/SubPage/AuthContext.js
@@ -1,18 +1,38 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [profileImage, setProfileImage] = useState('icon_boo_big.png'); // 프로필 이미지 상태 추가
+    const [profileImage, setProfileImage] = useState('icon_boo_big.png');
+
+    const fetchProfileImage = async (username) => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_IP}/get_profile`, {
+                params: { user_name: username },
+                responseType: 'blob'
+            });
+            if (response.status === 200) {
+                const imageUrl = URL.createObjectURL(response.data);
+                setProfileImage(imageUrl);
+            } else {
+                setProfileImage('icon_boo_big.png');
+            }
+        } catch (error) {
+            console.error("프로필 이미지 불러오기 실패", error);
+            setProfileImage('icon_boo_big.png');
+        }
+    };
 
     const login = (username) => {
-        setIsLoggedIn(username); // 로그인 시 사용자명을 저장
+        setIsLoggedIn(username);
+        fetchProfileImage(username);  // 로그인 시 프로필 이미지 로드
     };
 
     const logout = () => {
         setIsLoggedIn(false);
-        setProfileImage('icon_boo_big.png'); // 로그아웃 시 기본 프로필 이미지로 설정
+        setProfileImage('icon_boo_big.png');
     };
 
     return (
