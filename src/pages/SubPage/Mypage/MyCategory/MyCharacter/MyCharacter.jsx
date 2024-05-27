b233d28e315093a74fcdf3be57e3816dfbf9bd7a
nayeong lee <lny021102@gmail.com>
2024-05-27 18:37:54 +0900

    [ADD] 캐릭터 화면 1차 완성(닉네임 put, get API 연동 완료

--- src/pages/SubPage/Mypage/MyCategory/MyCharacter/MyCharacter.jsx
+++ src/pages/SubPage/Mypage/MyCategory/MyCharacter/MyCharacter.jsx
@@ -1,25 +1,12 @@
-// 서희님은 피그마 디자인을 바탕으로 이 컴포넌트를 만들어주세요!
-// Character는 나중에 개발할 기능이니까, Boo 캐릭터를 넣어서 '곧 (Character 기능을) 만나요~' 같은 느낌으로 꾸며주세요
-// import 되어있는 컴포넌트를 적극 활용하세요!
-// 이 컴포넌트는 마이페이지의 '캐릭터' 탭을 눌러서 볼 수 있어요.
-
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
-import CustomFont from '../../../../../Components/Container/CustomFont'; // 폰트 디자인 가능 컴포넌트
-// 사용예시: <CustomFont font='2rem' color='black' fontWeight='bold>여기에 글씨</CustomFont>
-//font에는 글싸의 크기를, color에는 색상을, fontWeight는 굵게하고 싶을 때 bold를 넣어 쓰세요!
-
-import CustomCenter from '../../../../../Components/Container/CustomCenter'; // 중앙 정렬 컴포넌트
-// 사용예시: <CustomCenter>중앙에 오게 만들고 싶은 아무 요소</CustomCenter>
-
-import CustomColumn from '../../../../../Components/Container/CustomColumn'; // 세로 정렬 가능 컴포넌트
-// 사용예시: <CustomColumn width='100%' alignItems='center' justifyContent='center'>세로 정렬하고 싶은 요소들</CustomColumn>
-// width에는 가로 길이를(부모 컴포넌트의 가로 길이에 비례해서 길어져요) alignItems에는 세로로 center/flex-start/flex-end 중 뭔지, justifyContent는 가로로~
-
-import CustomRow from '../../../../../Components/Container/CustomRow'; // 가로 정렬 가능 컴포넌트
-// 사용예시:  <CustomRow width='100%' alignItems='center' justifyContent='center'>세로 정렬하고 싶은 요소들</CustomRow>
-
-import StyledImg from '../../../../../Components/Container/StyledImg'; // 이미지 디자인 가능 컴포넌트
-// 사용얘시: <StyledImg src={'이미지 경로.확장자'} width='가로길이' height='세로길이' />
+import CustomFont from '../../../../../Components/Container/CustomFont';
+import CustomCenter from '../../../../../Components/Container/CustomCenter';
+import CustomColumn from '../../../../../Components/Container/CustomColumn';
+import CustomRow from '../../../../../Components/Container/CustomRow';
+import StyledImg from '../../../../../Components/Container/StyledImg';
+import { useAuth } from '../../../AuthContext';
 
 const ContainerCenter = styled.div`
   display: flex;
@@ -41,19 +28,140 @@ const PageContainer = styled(ContainerCenter)`
   background-color: white;
 `;
 
-export default function Component() {
+const IsValidButton = styled.button`
+  width: 30%;
+  height: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.7rem;
+  color: white;
+  border: none;
+  border-radius: 15px;
+  background-color: #C1EEA5;
+  cursor: pointer;
+`;
+
+const InputForm = styled.input`
+  display: flex;
+  border: 1.5px solid #8CC63F;
+  background-color: transparent;
+  border-radius: 15px;
+  width: 100%;
+  height: 2rem;
+  padding: 0.3rem;
+
+  &::placeholder {
+    color: #D9D9D9;
+  }
+
+  &:active {
+    outline: none;
+  }
+`;
+
+export default function CharacterComponent() {
+  const [documentCount, setDocumentCount] = useState(0);
+  const [nickname, setNickname] = useState('');
+  const [newNickname, setNewNickname] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
+  const { isLoggedIn, profileImage, setProfileImage } = useAuth();
+  const username = isLoggedIn;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (username) {
+      setLoading(true);
+      fetchDocumentCount();
+      fetchNickname().finally(() => setLoading(false));
+    }
+  }, [username]);
+
+  useEffect(() => {
+    console.log('nickname 상태 업데이트:', nickname);
+  }, [nickname]);
+
+  const fetchDocumentCount = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_IP}/document_create_count`, {
+        params: { user_name: username },
+      });
+      setDocumentCount(response.data.count);
+    } catch (error) {
+      console.error('문서 생성 수 불러오기 실패', error);
+    }
+  };
+
+  const fetchNickname = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_IP}/get_nickname`, {
+        params: { user_name: username },
+      });
+      console.log('닉네임 조회 성공:', response.data);
+      const fetchedNickname = response.data;
+      // 닉네임이 존재하며, 빈 문자열이 아닐 경우 기본값을 설정하는 부분이다
+      setNickname(fetchedNickname && fetchedNickname.trim() !== '' ? fetchedNickname : 'Boo의 이름을 지어주세요.');
+    } catch (error) {
+      console.error('닉네임 불러오기 실패', error);
+      setNickname('Boo의 이름을 지어주세요.');
+    }
+  };
+
+  const updateNickname = async () => {
+    try {
+      const response = await axios.put(`${process.env.REACT_APP_SERVER_IP}/change_nick_name`, null, {
+        params: {
+          user_name: username,
+          nick_name: newNickname,
+        }
+      });
+      console.log('닉네임 변경 성공:', response.data);
+      setNickname(newNickname);
+      setIsEditing(false);
+    } catch (error) {
+      console.error('닉네임 변경 실패', error);
+    }
+  };
+
+  const getImageBasedOnDocumentCount = () => {
+    if (documentCount >= 1 && documentCount < 20) return 'Boo_grow_1.png';
+    if (documentCount >= 20 && documentCount < 40) return 'Boo_grow_2.png';
+    if (documentCount >= 40 && documentCount < 60) return 'Boo_grow_3.png';
+    if (documentCount >= 60 && documentCount < 80) return 'Boo_grow_4.png';
+    if (documentCount >= 80 && documentCount < 100) return 'Boo_grow_5.png';
+    if (documentCount >= 100 && documentCount < 120) return 'Boo_grow_6.png';
+    if (documentCount >= 120 && documentCount < 140) return 'Boo_grow_7.png';
+    if (documentCount >= 140) return 'Boo_grow_8.png';
+    return 'Boo_grow_1.png';
+  };
+
   return (
     <ContainerCenter>
       <PageContainer>
-      <CustomCenter>
-          <StyledImg src={'icon_boo.png'} width='500px' height='480px' borderRadius='20%' />
-        </CustomCenter>
-
-        <CustomFont color='skyblue' fontWeight='bold' font='50px'>Coming Soon...!</CustomFont>
-        <CustomFont color='black' fontWeight='thin' font='20px'>보다 나은 서비스 제공을 위하여 페이지 준비중에 있습니다.</CustomFont>
-        <CustomFont color='black' fontWeight='thin' font='20px'>빠른 시일 내에 만나요!</CustomFont>
-
+        <StyledImg src={getImageBasedOnDocumentCount()} width='200px' height='200px' />
+        {loading ? (
+          <p>Loading...</p>
+        ) : isEditing ? (
+          <CustomColumn width='80%' alignItems='center' gap='10px'>
+            <InputForm placeholder='새 닉네임 입력' value={newNickname} onChange={(e) => setNewNickname(e.target.value)} />
+            <CustomRow width='100%' justifyContent='space-around'>
+              <IsValidButton onClick={updateNickname}>확인</IsValidButton>
+              <IsValidButton onClick={() => setIsEditing(false)}>취소</IsValidButton>
+            </CustomRow>
+          </CustomColumn>
+        ) : (
+          <CustomColumn width='80%' alignItems='center' justifyContent='center' gap='10px'>
+            <IsValidButton onClick={() => setIsEditing(true)}>
+              <CustomRow width='100%' justifyContent='space-around'>
+                <CustomFont color='#8CC63F' font='1rem' fontWeight='bold'>
+                  {nickname}
+                </CustomFont>
+                <StyledImg src={'icon_feather.png'} width='30px' height='30px' />
+              </CustomRow>
+            </IsValidButton>
+          </CustomColumn>
+        )}
       </PageContainer>
     </ContainerCenter>
   );
-};
+}
